c6acafe5f82c13fcb03902c9629cd1dcd04c888b
Max Sidenstjärna <1145070+eco37@users.noreply.github.com>
2025-12-03 12:21:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
 from setuptools import setup
+from setuptools.command.install import install
+from subprocess import check_call
+
+
+class PreInstallCommand(install):
+    """Pre-installation for installation mode."""
+    def run(self):
+        check_call("apt-get install pkg-config libboost-python-dev libboost-thread-dev libbluetooth-dev libglib2.0-dev python3-dev".split())
+        install.run(self)
+
 
 setup(name='cbpi4-tilt-pro',
       version='0.0.1',
-      description='CraftBeerPi Plugin',
-      author='',
+      description='CraftBeerPi4 Plugin for Tilt Pro',
+      author='Max Sidenstjärna',
       author_email='',
-      url='',
+      url='https://github.com/eco37/cbpi4-tilt-pro',
       include_package_data=True,
       package_data={
         # If any package contains *.txt or *.rst files, include them:
       '': ['*.txt', '*.rst', '*.yaml'],
       'cbpi4-tilt-pro': ['*','*.txt', '*.rst', '*.yaml']},
       packages=['cbpi4-tilt-pro'],
+      install_requires=[
+      'PyBluez @ https://github.com/AcrossTheCloud/pybluez/archive/master.zip#egg=PyBluez-0.30',
+      'gattlib-dbus @ https://github.com/oscaracena/pygattlib/archive/dbus.zip#egg=gattlib-dbus-24.1.8',
+      ]
      )
