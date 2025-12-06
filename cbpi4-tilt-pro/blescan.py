9c6b415eced8d8a65cb99df9f19125c677586a2f
Max S <>
2025-12-06 11:59:33 +0100

    Update

--- cbpi4-tilt-pro/blescan.py
+++ cbpi4-tilt-pro/blescan.py
@@ -194,7 +194,7 @@ def parse_events(sock, loop_count=100):
 
     print(beacons)
 
-    sock.setsockopt( bluez.SOL_HCI, bluez.HCI_FILTER, old_filter )
+    #sock.setsockopt( bluez.SOL_HCI, bluez.HCI_FILTER, old_filter )
     print("Return")
     return beacons
 
